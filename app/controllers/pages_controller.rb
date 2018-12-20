23fa0bc9c48b2fde22e9df732d3e6c99d3803c89
Celso Silva <silva.celso29@gmail.com>
2018-12-19 19:49:09 -0500

    customized portfolio page slideshow, added routes for about page

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,7 +1,11 @@
 class PagesController < ApplicationController
+
   def welcome
   end
 
   def portfolio
+  end
+
+  def about
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -3,4 +3,6 @@ Rails.application.routes.draw do
   root 'pages#welcome'
 
   get '/portfolio' => 'pages#portfolio'
+
+  get '/about' => 'pages#about'
 end
